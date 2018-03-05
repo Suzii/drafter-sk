72d2e741de234cbf46418ce650f9f5c455d10598
Suzii <z.dankovcikova@gmail.com>
2018-03-05 22:21:00 +0100

    Add correct contact data

--- app/scripts/productsApp/components/ProductsListing.tsx
+++ app/scripts/productsApp/components/ProductsListing.tsx
@@ -2,6 +2,12 @@ import * as React from 'react';
 import { Product } from '../../models/Product';
 import { ProductPreview } from './ProductPreview';
 
+const ProductsEmpty = () => (
+  <div className="products__empty-message col-xs-12 center-xs">
+    <h3>Neboli nájdené žiadne produkty vyhovújuce filtru...</h3>
+  </div>
+);
+
 interface IProps {
   products: Product[],
 }
@@ -10,9 +16,8 @@ export const ProductsListing: React.SFC<IProps> = ({ products }) => {
   return (
     <div className="row products__items">
       {products && products.length
-        ? products.map((product: Product) =>
-          <ProductPreview key={product.system.codename} product={product} />)
-        : 'Neboli najdene ziadne produkty vyhovujuce filtru...'
+        ? products.map((product: Product) => <ProductPreview key={product.system.codename} product={product} />)
+        : <ProductsEmpty />
       }
     </div>
   );
