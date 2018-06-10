1a42a9ebe8f2175546390b61ba453c00a6627ebc
Suzii <z.dankovcikova@gmail.com>
2018-06-10 19:15:42 +0200

    Add smooth scrolling to anchor tags

--- app/scripts/index.ts
+++ app/scripts/index.ts
@@ -4,10 +4,13 @@ if (process.env.NODE_ENV !== 'production') {
   require('./../index.pug')
 }
 
-(window as any).FontAwesomeConfig = { autoReplaceSvg: 'nest' }
+(window as any).FontAwesomeConfig = { autoReplaceSvg: 'nest' };
 
 // bootstrap react apps
 import './productsApp/index.tsx';
 
-// Other hacks on page
-// document.getElementById('footer__copyright--year').innerText = new Date().getFullYear().toString();
+// ====== Other hacks on page =========
+document.getElementById('footer__copyright--year').innerText = new Date().getFullYear().toString();
+
+import * as SmoothScroll from 'smooth-scroll';
+new SmoothScroll('a[href*="#"]');
